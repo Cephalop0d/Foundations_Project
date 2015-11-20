0ea07bad723c56a11d8d6040844d3dd4dc74c4ba
Angela_Les <angiemles@gmail.com>
2015-11-20 15:20:41 -0600

    Project
    Includes initial merge (to relate # of PH in previous hour to change in
    party composition)

---
+++ project.R
@@ -0,0 +1,8 @@
+rm(list=ls())
+getwd()
+data <- read.csv("data_clean.csv")
+
+data.next <- data
+data.next$hour.next <- data.next$Hour+1
+hist(data.next$hour.next)
+data.merge <- merge(data, data.next, by.x = c("Party","Hour"), by.y = c("Party","hour.next"), all.x=T)
